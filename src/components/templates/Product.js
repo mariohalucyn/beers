e73077aa602d1fd0f6cd4b032269205179592c77
Kacper Gajda <lorenstenlelak@gmail.com>
2023-05-24 23:23:00 +0200

    simple improvements

--- src/components/templates/Product.js
+++ src/components/templates/Product.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../molecules/Header/Header';
 import Footer from '../molecules/Footer/Footer';
+import Item from '../organisms/Item/Item';
 
 const Product = () => (
 	<>
 		<Header />
+		<Item />
 		<Footer />
 	</>
 );
