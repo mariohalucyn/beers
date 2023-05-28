2a748c0322a5e09632ec7ebcac620e747d4b9926
Kacper Gajda <lorenstenlelak@gmail.com>
2023-05-28 14:13:01 +0200

    routing sketch completed

--- src/index.js
+++ src/index.js
@@ -2,14 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Root from './views/Root';
-import { ThemeProvider } from 'styled-components';
-import { theme } from './assets/themes/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<ThemeProvider theme={theme}>
-			<Root />
-		</ThemeProvider>
+		<Root />
 	</React.StrictMode>
 );
--- src/views/Root.js
+++ src/views/Root.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import styled from 'styled-components';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import List from '../components/templates/List';
+import Product from '../components/templates/Product';
+import { ThemeProvider } from 'styled-components';
+import { theme } from '../assets/themes/theme';
 
 const StyledWrapper = styled.html `
 	color: ${({ theme }) => theme.colors.black}
 `;
 
-const Root = () => (
-	<StyledWrapper>
-		<List />
-	</StyledWrapper>
-);
-
+const Root = () => {
+	return (
+		<Router>
+			<ThemeProvider theme={theme}>
+				<StyledWrapper>
+					<ul>
+						<li>
+							<Link to='/'>home</Link>
+						</li>
+						<li>
+							<Link to='/beer'>beers</Link>
+						</li>
+					</ul>
+					<Routes>
+						<Route exact path='/' element={<List />}></Route>
+						<Route exact path='/beer' element={<Product />}></Route>
+					</Routes>
+				</StyledWrapper>
+			</ThemeProvider>
+		</Router>
+	);
+};
 export default Root;
