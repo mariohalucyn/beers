8be75e2bd89bb5e30b86c50545205f529eaacfdf
Kacper Gajda <lorenstenlelak@gmail.com>
2023-06-03 15:09:03 +0200

    basic contact page

--- src/components/organisms/Contact/Contact.js
+++ src/components/organisms/Contact/Contact.js
@@ -1,9 +1,31 @@
 import React from 'react'
 import { StyledWrapper } from '../../atoms/StyledWrapper'
+import { StyledContact } from './Contact.styles'
+import Mushroom from '../../../assets/pictures/mario mushroom.png'
 
 const Contact = () => (
   <StyledWrapper>
-    <h1>Contact</h1>
+    <StyledContact>
+      <div>
+        <h1>Kacper Gajda</h1>
+        <h3>kgajda226@gmail.com</h3>
+        <p>
+          {/* eslint-disable-next-line react/no-unescaped-entities */}
+          I'm a hobbyist programmer now I'm learning JavaScript actually react
+          and this is example of site you can make with this framework (or
+          actually library)
+        </p>
+        <ul>
+          links to my socials
+          <li>https://github.com/mariohalucyn</li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+      <div>
+        <img src={Mushroom} alt="mushroom profile picture" />
+      </div>
+    </StyledContact>
   </StyledWrapper>
 )
 
---
+++ src/components/organisms/Contact/Contact.styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const StyledContact = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: 1fr;
+  gap: 82px;
+
+  img {
+    max-width: 500px;
+  }
+`
