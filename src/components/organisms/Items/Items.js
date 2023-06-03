e312f60f60231fc1a7dec50a761c1c83eb5161f3
Kacper Gajda <lorenstenlelak@gmail.com>
2023-06-03 14:35:16 +0200

    sorting removed

--- src/components/organisms/Items/Items.js
+++ src/components/organisms/Items/Items.js
@@ -5,61 +5,36 @@ import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { StyledList, StyledListItem } from './Items.styles'
 import { StyledWrapper } from '../../atoms/StyledWrapper'
-import styled from 'styled-components'
-
-const StyledSelectDiv = styled.div`
-  display: flex;
-  justify-content: end;
-  align-items: center;
-  gap: 4px;
-  padding-bottom: 16px;
-`
-
-const StyledSelect = styled.select`
-  margin: 0;
-  padding: 0;
-  color: ${({ theme }) => theme.colors.black};
-`
 
 const Items = (props) => {
   const [...data] = fetchApi('https://api.punkapi.com/v2/beers')
-  // const [sortState, setSortState] = useState('none')
 
   return (
     <StyledWrapper>
-      <StyledSelectDiv>
-        sort
-        <StyledSelect defaultValue={'DEFAULT'}>
-          <option value="DEFAULT" disabled>
-            None
-          </option>
-          <option value="ascending">Ascending</option>
-          <option value="descending">Descending</option>
-        </StyledSelect>
-      </StyledSelectDiv>
-
       <StyledList>
-        {data.map(({ abv, brewers_tips, id, image_url, name, tagline }) => (
-          // destructuring all necessary fields
-          <StyledListItem key={id}>
-            <div>
-              <Link onClick={() => props.func(id)} to="/beer">
-                <h1>{name}</h1>
-              </Link>
-              <h3>{tagline}</h3>
-            </div>
-            <img src={image_url} alt={name} />
-            <div>
-              <p>{brewers_tips}</p>
-              <div className={'price'}>
-                <h1>{abv}%</h1>
+        {[...data].map(
+          ({ abv, brewers_tips, id, image_url, name, tagline }) => (
+            // destructuring all necessary fields
+            <StyledListItem key={id}>
+              <div>
                 <Link onClick={() => props.func(id)} to="/beer">
-                  <Button>Read</Button>
+                  <h1>{name}</h1>
                 </Link>
+                <h3>{tagline}</h3>
               </div>
-            </div>
-          </StyledListItem>
-        ))}
+              <img src={image_url} alt={name} />
+              <div>
+                <p>{brewers_tips}</p>
+                <div className={'price'}>
+                  <h1>{abv}%</h1>
+                  <Link onClick={() => props.func(id)} to="/beer">
+                    <Button>Read</Button>
+                  </Link>
+                </div>
+              </div>
+            </StyledListItem>
+          )
+        )}
       </StyledList>
     </StyledWrapper>
   )
